0185737adc4cb07e0d9c3f4cec8ff078765966fd
Linberg34 <pfqxbr2208@gmail.com>
2025-04-02 17:15:19 +0700

    Init commit + copied svgs, doing architechture

---
+++ lk-stud/src/app/app.tsx
@@ -0,0 +1,10 @@
+import "../shared/styles/fonts.css"
+import "../shared/styles/typography.css"
+
+export const App = () => {
+    return (
+        <div>
+            <h1>lk-stud</h1>
+        </div>
+    )
+}
